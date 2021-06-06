6ed09fc335859db876b65f55a8099c88b2b77901
gongcu <knmy0101@naver.com>
2021-06-06 13:37:26 +0900

    test: unit test example 1. Repository의 getCenter가 잘 동작하는지 확인하는 유닛 테스트 -> BluemapAAPI를 mock으로 주입 -> assertThat의 matcher 파라미터에 isA를 활용하여 실제 반환 타입이 같은지 확인

--- app/src/main/java/com/bluemap/overcom_blue/di/AppModule.kt
+++ app/src/main/java/com/bluemap/overcom_blue/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.bluemap.overcom_blue.di
 
 import com.bluemap.overcom_blue.network.BluemapAPI
+import com.bluemap.overcom_blue.user.UserManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,4 +27,8 @@ object AppModule {
     @Provides
     @Singleton
     fun provideApiService(retrofit: Retrofit) = retrofit.create(BluemapAPI::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserManager() = UserManager
 }
--- app/src/test/java/com/bluemap/overcom_blue/ExampleUnitTest.kt
+++ app/src/test/java/com/bluemap/overcom_blue/ExampleUnitTest.kt
@@ -1,17 +1,53 @@
 package com.bluemap.overcom_blue
 
+import com.bluemap.overcom_blue.model.Center
+import com.bluemap.overcom_blue.model.Location
+import com.bluemap.overcom_blue.model.Post
+import com.bluemap.overcom_blue.network.BluemapAPI
+import com.bluemap.overcom_blue.repository.Repository
+import com.bluemap.overcom_blue.user.UserManager
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.internal.operators.single.SingleSubscribeOn
+import io.reactivex.schedulers.Schedulers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.isA
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.any
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
+import javax.inject.Inject
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+@RunWith(MockitoJUnitRunner::class)
+class ApiUnitTest {
+
+    @Mock
+    lateinit var bluemapAPI: BluemapAPI //Mock 객체 생성
+
+    @InjectMocks
+    lateinit var repository: Repository //Repository에 Mocking한 객체(BluemapAPI,UserManager) 주입
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun getCenters(){
+        //MockitoAnnotations.initMocks(this)
+        val location  =  Location(37.570975,126.977759)
+        val center = mock(Center::class.java)
+        val temp = Single.just(listOf(center) as List<Center>)
+        Mockito.`when`(bluemapAPI.getCenter(location.latitude,location.longitude)).thenReturn(temp)
+        assertThat(repository.getCenter(location),`isA`(Single::class.java))
     }
 }
